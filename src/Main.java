97e5ce1adb343687474cd24da49406952a2ae687
Timur-Gus <timurka24478@gmail.com>
2023-11-16 16:53:54 +0300

    Билет 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
     public static void main(String[] args) {
         String palindrome = "А роза упала на лапу Азора";
         String string = "Пример не палиндрома";
+        isPalindrome(palindrome);
+        isPalindrome(string);
+    }
+    public static void isPalindrome(String str) {
+        if (new StringBuilder(str).reverse().toString().
+                replace(" ", "").
+                compareToIgnoreCase(str.replace(" ", "")) == 0) {
+            System.out.println("Строка является палиндромом");
+        }
+        else {
+            System.out.println("Строка не является палиндромом");
+        }
     }
 }
