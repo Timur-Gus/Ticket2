58dd01aab4607e3b9afcfcff7760474a01733af6
Timur-Gus <timurka24478@gmail.com>
2023-11-16 16:45:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String palindrome = "А роза упала на лапу Азора";
+        String string = "Пример не палиндрома";
+    }
+}
